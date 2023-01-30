f87b0300c9bcc0b7f125cdbf42576c7b643a2139
Vladimir Komarov <vvkcrtc@gmail.com>
2023-01-30 21:13:30 +0300

    Workable version

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -5,10 +5,11 @@ package ru.netology;
 //import java.nio.file.Files;
 //import java.nio.file.Path;
 //import java.time.LocalDateTime;
+import java.io.IOException;
 import java.util.List;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
 
     Server server = new Server(9999, validPaths);
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,47 +1,136 @@
 package ru.netology;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class Server {
     protected static List<String> validPaths = new ArrayList<>();
     protected static int serverPort;
+    ServerSocket server = null;
 
     public Server(int serverPort, List<String> validPaths) {
-        Server.serverPort = serverPort;
-        Server.validPaths.addAll(validPaths);
+        this.serverPort = serverPort;
+        this.validPaths.addAll(validPaths);
     }
 
-    public static void run() {
-        try (final var serverSocket = new ServerSocket(serverPort)) {
+
+    public void run() throws IOException {
+
+        try {
+            server = new ServerSocket(serverPort);
+            server.setReuseAddress(true);
+
+            ExecutorService executorService = Executors.newFixedThreadPool(64);
+
             while (true) {
-                try (
-                        final var socket = serverSocket.accept();
-                        final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        final var out = new BufferedOutputStream(socket.getOutputStream())
-                ) {
 
-                    var connectHandler = new ConnectHandler(socket, in, out, validPaths);
-                    Thread connectThread = new Thread(connectHandler);
-                    connectThread.start();
+                Socket client = server.accept();
+/*
+                System.out.println("New client connected"
+                        + client.getInetAddress()
+                        .getHostAddress());
+*/
+                executorService.execute(new ClientHandler(client));
 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                try {
+                    server.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                  /*
+            }
+        }
+    }
 
-                    // read only request line for simplicity
-                    // must be in form GET /path HTTP/1.1
+    // ClientHandler class
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        // Constructor
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        protected static boolean checkCorrectPath(BufferedOutputStream out, String path) throws IOException {
+            if (!validPaths.contains(path)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                return false;
+            }
+            return true;
+        }
+
+        protected static void sendClassic(BufferedOutputStream out, Path filePath, String mimeType) throws IOException {
+            final var template = Files.readString(filePath);
+            final var content = template.replace(
+                    "{time}",
+                    LocalDateTime.now().toString()
+            ).getBytes();
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.write(content);
+            out.flush();
+        }
+
+        protected static void sendFile(BufferedOutputStream out, Path filePath, String mimeType) throws IOException {
+            final var length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+
+        }
+
+        public void run() {
+            BufferedOutputStream out = null;
+            BufferedReader in = null;
+            try {
+
+                out = new BufferedOutputStream(clientSocket.getOutputStream());
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                while (true) {
                     final var requestLine = in.readLine();
+                    System.out.println("Request line :" + requestLine);
+
+                    if (requestLine == null) {
+                        break;
+                        //continue;
+                    }
                     final var parts = requestLine.split(" ");
+                    System.out.println("Parts :");
+                    for (int i = 0; i < parts.length; i++) {
+                        System.out.print(" " + parts[i]);
+                    }
+                    System.out.println(" ");
 
                     if (parts.length != 3) {
                         // just close socket
@@ -49,14 +138,7 @@ public class Server {
                     }
 
                     final var path = parts[1];
-                    if (!validPaths.contains(path)) {
-                        out.write((
-                                "HTTP/1.1 404 Not Found\r\n" +
-                                        "Content-Length: 0\r\n" +
-                                        "Connection: close\r\n" +
-                                        "\r\n"
-                        ).getBytes());
-                        out.flush();
+                    if (!checkCorrectPath(out, path)) {
                         continue;
                     }
 
@@ -65,40 +147,27 @@ public class Server {
 
                     // special case for classic
                     if (path.equals("/classic.html")) {
-                        final var template = Files.readString(filePath);
-                        final var content = template.replace(
-                                "{time}",
-                                LocalDateTime.now().toString()
-                        ).getBytes();
-                        out.write((
-                                "HTTP/1.1 200 OK\r\n" +
-                                        "Content-Type: " + mimeType + "\r\n" +
-                                        "Content-Length: " + content.length + "\r\n" +
-                                        "Connection: close\r\n" +
-                                        "\r\n"
-                        ).getBytes());
-                        out.write(content);
-                        out.flush();
+                        sendClassic(out, filePath, mimeType);
                         continue;
                     }
 
-                    final var length = Files.size(filePath);
-                    out.write((
-                            "HTTP/1.1 200 OK\r\n" +
-                                    "Content-Type: " + mimeType + "\r\n" +
-                                    "Content-Length: " + length + "\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    Files.copy(filePath, out);
-                    out.flush();
+                    sendFile(out, filePath, mimeType);
                 }
-
-                */
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                    if (in != null) {
+                        in.close();
+                        clientSocket.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
     }
 }
